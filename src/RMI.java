319d57dc0d93fb82b1efcb1a386727627bb7e991
Karolina <eszkarolina@gmail.com>
2015-06-15 00:39:21 +0200

    Server start

---
+++ src/RMI.java
@@ -0,0 +1,18 @@
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author karolina
+ */
+public interface RMI extends Remote {
+    public String getData(String s) throws RemoteException;
+    
+}
---
+++ src/RMIServer.java
@@ -0,0 +1,37 @@
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author karolina
+ */
+public class RMIServer extends UnicastRemoteObject implements RMI{
+  
+    public RMIServer() throws RemoteException{
+        super();
+    }
+    public String getData(String s) throws RemoteException{
+        s = "Hi"+s;
+        return s;
+    }
+    
+    public static void main(String args[]){
+        try{
+            Registry reg  = LocateRegistry.createRegistry(1099);
+            reg.rebind("server", new RMIServer());
+            System.out.println("Server wystartował");
+            
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
